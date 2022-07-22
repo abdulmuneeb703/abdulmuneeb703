bf1dae6372c1e67cd7746e32f47cc46e9e8cbf4a
abdulmuneeb703 <109253420+abdulmuneeb703@users.noreply.github.com>
2022-07-22 10:08:26 -0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,10 @@
+# Abdul Muneeb Syed
+# 7/8/2022
+#This code calculates the speed in kmh/mph
+# To enter the kilometers per hour as input
+kmh = int(input("Enter km/h: "))
+# Speed in miles can be converted by multiplying with 0.6214
+mph =  0.6214 * kmh
+# The speed is calculated and printed accordingly
+print("Speed:", kmh, "KM/H = ", mph, "MPH")
+
